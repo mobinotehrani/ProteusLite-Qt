bf233dacc004c57d75291f0a59a6e60fbae87faa
Reyhaneh Aghamirzaei <reyhaneh.aghamirzaei@gmail.com>
2026-07-13 20:08:08 +0330

    Add canvas workspace with grid and zoom
    Implemented the interactive design canvas with grid rendering, snap support, zoom controls, panning, and workspace status indicators.

--- mainwindow.h
+++ mainwindow.h
@@ -6,8 +6,10 @@
 #include <QMainWindow>
 
 class QAction;
+class CanvasView;
 class QLabel;
 class QMenu;
+class QStackedWidget;
 
 class MainWindow final : public QMainWindow
 {
@@ -16,7 +18,12 @@ public:
 
 private:
     void buildInterface();
+    void buildWorkspaceActions();
     void buildMenus();
+    void buildToolBar();
+    void buildStatusBar();
+    void connectCanvasSignals();
+
     void showStartMenu();
     void createNewProject(const QString &name, const QSize &canvasSize);
     void browseAndOpenProject();
@@ -25,12 +32,29 @@ private:
     void refreshRecentMenu();
     void showProject(const ProjectMetadata &metadata);
     void showNoProject();
+    void setWorkspaceActionsEnabled(bool enabled);
+    void updateWindowTitle();
 
     RecentProjectsManager m_recentProjects;
     ProjectMetadata m_currentProject;
-    QWidget *m_workspace{};
+
+    QStackedWidget *m_workspaceStack{};
+    QWidget *m_emptyPage{};
+    QWidget *m_canvasPage{};
     QLabel *m_projectTitle{};
     QLabel *m_projectDetails{};
-    QLabel *m_canvasPlaceholder{};
+    CanvasView *m_canvasView{};
     QMenu *m_recentMenu{};
+
+    QAction *m_gridAction{};
+    QAction *m_snapAction{};
+    QAction *m_zoomInAction{};
+    QAction *m_zoomOutAction{};
+    QAction *m_resetZoomAction{};
+    QAction *m_fitSheetAction{};
+
+    QLabel *m_coordinateStatus{};
+    QLabel *m_snapStatus{};
+    QLabel *m_zoomStatus{};
+    QLabel *m_panStatus{};
 };
